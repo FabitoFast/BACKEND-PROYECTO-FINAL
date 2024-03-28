e8bde406d77909cd365e6801c16f44c947f83c48
FabitoFast <fabioantunez@gmail.com>
2024-03-28 15:24:08 -0300

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-require('dotenv').config();
+const axios = require('axios');
+//const path = require('path');
+//const cookieParser = require('cookie-parser');
+//const logger = require('morgan');
+require('dotenv').config(); 
+const Componente = require('./models/Componente');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const gameRouter = require('./routes/game');
 const {dbConnection} = require('./db/db')
 
 const app = express();
 
-app.use(logger('dev'));
+//MIDLEWARE PARA ASISTIR EN RESPUESTA Y/O PETICION EN JSON //
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use(logger('dev'));
+
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+
+  
 dbConnection();
 
 module.exports = app;
