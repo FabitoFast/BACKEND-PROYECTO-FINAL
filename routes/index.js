620869b99ee391169db740168f30713c69dea445
FabitoFast <fabioantunez@gmail.com>
2024-03-28 15:33:30 -0300

    routes/index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/controller');
-
-/* GET home page. */
-router.get('/', controller.myIndex);
-
-
-module.exports = router;
